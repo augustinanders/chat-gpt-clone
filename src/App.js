44decbba08ec2707feb0e9a192f4dd908961ddc0
Augustin Anders <augustinanders@gmail.com>
2023-10-16 12:59:09 +0200

    style main section

--- src/App.js
+++ src/App.js
@@ -12,7 +12,21 @@ function App() {
         </nav>
       </section>
 
-      <section className="main"></section>
+      <section className="main">
+        <h1>Gustl GPT</h1>
+        <ul className="feed"></ul>
+        <div className="bottom-section">
+          <div className="input-container">
+            <input />
+            <div id="submit">➢</div>
+          </div>
+          <p className="info">
+            Chat GPT. Free Research Preview. Our goal is to make AI systems more
+            natural and safe to interact with. Your feedback will help us
+            improve.
+          </p>
+        </div>
+      </section>
     </div>
   );
 }
